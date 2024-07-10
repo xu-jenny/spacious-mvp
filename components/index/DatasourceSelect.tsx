2d448d7748bef9fcf267cc18931909baaef6cf44
Xingyu Wang <44791063+KoiOne@users.noreply.github.com>
2024-07-10 00:54:32 -0400

    Change data source text

--- components/index/DatasourceSelect.tsx
+++ components/index/DatasourceSelect.tsx
@@ -6,22 +6,34 @@ import { USDatasetSource } from "./EditTagButton";
 
 type Props = {
     dataSource: USDatasetSource | null;
-  setDataSource: Dispatch<SetStateAction<USDatasetSource>>;
-}
+    setDataSource: Dispatch<SetStateAction<USDatasetSource>>;
+};
 const DatasourceSelect = ({ dataSource, setDataSource }: Props) => {
-  return (
-    <div className="p-3 mt-3">
-      <span>Specify datasource</span>
-      <Dropdown label={dataSource ?? "Any"} inline>
-        <Dropdown.Item onClick={() => setDataSource('ANY')}>Any</Dropdown.Item>
-        <Dropdown.Item onClick={() => setDataSource('PFAS')}>PFAS</Dropdown.Item>
-        <Dropdown.Item onClick={() => setDataSource('USGS')}>USGS</Dropdown.Item>
-        <Dropdown.Item onClick={() => setDataSource('LASERFICHE')}>LaserFiche</Dropdown.Item>
-        <Dropdown.Item onClick={() => setDataSource('USGOV')}>US Gov</Dropdown.Item>
-        <Dropdown.Item onClick={() => setDataSource('NYOPEN')}>Ny Open Data</Dropdown.Item>
-      </Dropdown>
-    </div>
-  );
+    return (
+        <div className="p-3 mt-3">
+            <span>Specify Data Source</span>
+            <Dropdown label={dataSource ?? "Any"} inline>
+                <Dropdown.Item onClick={() => setDataSource("ALL")}>
+                    ALL
+                </Dropdown.Item>
+                <Dropdown.Item onClick={() => setDataSource("PFAS")}>
+                    PFAS
+                </Dropdown.Item>
+                <Dropdown.Item onClick={() => setDataSource("USGS")}>
+                    USGS
+                </Dropdown.Item>
+                <Dropdown.Item onClick={() => setDataSource("NC DEQ")}>
+                    NC DEQ
+                </Dropdown.Item>
+                <Dropdown.Item onClick={() => setDataSource("USGOV")}>
+                    US Gov
+                </Dropdown.Item>
+                <Dropdown.Item onClick={() => setDataSource("NY Open Data")}>
+                    NY Open Data
+                </Dropdown.Item>
+            </Dropdown>
+        </div>
+    );
 };
 
 export default DatasourceSelect;
