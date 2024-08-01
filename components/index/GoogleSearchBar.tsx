d4f62c8e09257bc37072153e56c7379fa97f32b8
xu-jenny <42222039+xu-jenny@users.noreply.github.com>
2024-07-31 23:34:56 -0400

    remove log message fro. GoogleSearchBar.tsx

--- components/index/GoogleSearchBar.tsx
+++ components/index/GoogleSearchBar.tsx
@@ -48,10 +48,6 @@ const GoogleSearchBar: React.FC<Props> = ({
         }
       });
     };
-    console.log(
-      "Google Maps API Key:",
-      process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
-    );
     if (!window.google) {
       loadScript(
         `https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`,
